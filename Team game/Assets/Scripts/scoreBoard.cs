5e58a0ff66cc9743b30ac80a56b67edcee671c9a
Flaxenfire <91972005+Flaxenfire@users.noreply.github.com>
2022-04-01 00:03:15 +0100

    Previous version deleted
    Removal of previous files

--- Team game/Assets/Scripts/scoreBoard.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-/*To let the round variable become the global variable */
-public static class scoreBoard
-{
-    public static int round = 0;
-}
